6aa5839462c032435ac542bd51a26a1d645dd650
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2014-08-14 12:00:13 +0300

    use Brackets events instead of intervals

--- viewmodels/documents.js
+++ viewmodels/documents.js
@@ -3,18 +3,13 @@ var DocumentManager = require('document/DocumentManager');
 
 define(function(require, exports, module){
     var ko = require('../vendor/knockout'),
-        AppInit = brackets.getModule('utils/AppInit');
+        _ = require('../vendor/lodash'),
+        $DocumentManager = $(DocumentManager);
     
     function DocumentsViewModel(){
         var self = this;
         this.documents = ko.observableArray([]);
         this.selected = ko.observable(null);
-        
-        function onTimeHandler(){
-            self.documents(self.getWorkingSet());
-            self.selected(self.getCurrentDocument());
-            setTimeout(onTimeHandler, 1000);
-        }
 
         this.onDocumentClick = function(model){
             DocumentManager.getDocumentForPath(model._path)
@@ -33,7 +28,44 @@ define(function(require, exports, module){
             event.stopPropagation();
         }
 
-        AppInit.appReady(onTimeHandler);
+        $DocumentManager.on('workingSetAdd', function(event, file){
+            self.documents.push(file);
+        });
+
+        $DocumentManager.on('workingSetRemove', function(event, file){
+            self.documents.remove(file);
+        });
+
+        $DocumentManager.on('currentDocumentChange', function(event, newDocument){
+            if (!newDocument){
+                return;
+            }
+            self.selected(newDocument.file);
+        });
+
+        $DocumentManager.on('workingSetAddList', function(event, files){
+            _.each(files, function(file){
+                self.documents.push(file);
+            });
+        });
+
+        $DocumentManager.on('workingSetRemoveList', function(event, files){
+            _.each(files, function(file){
+                self.documents.remove(file);
+            });
+        });
+
+        $DocumentManager.on('fileNameChange', _.bind(this.handlePathChanges, this));
+
+        $DocumentManager.on('pathDeleted', _.bind(this.handlePathChanges, this));
+    }
+
+    DocumentsViewModel.prototype.handlePathChanges = function(){
+        this.documents([]);
+        this.selected(null);
+
+        this.documents(this.getWorkingSet());
+        this.selected(this.getCurrentDocument());
     }
     
     DocumentsViewModel.prototype.getWorkingSet = function(){
