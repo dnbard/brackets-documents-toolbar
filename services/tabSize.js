ebd818013403731ceca322b752be3bec7262e5f8
Alex Bardanov <dnbard@gmail.com>
2014-12-24 21:44:17 +0200

    auto-resize bug fixed, close #44

--- services/tabSize.js
+++ services/tabSize.js
@@ -14,58 +14,16 @@ define(function(require, exports, module){
     function TabSizeService(){
         var self = this;
 
+        this.zoom = 1;
+
         this.lastResize = new Date(1,1,1);
         this.tabResizeWorker = function(supressRatioCheck){
-            var docs = $('.document-holder .document-name'),
-                documentsNameWidth = 0,
-                ratio = ($('.documents-holder').width() - $('.document:first').width() - 20) / $('.document-holder').width(),
-                docHolderWidth = $('.document-holder').width(),
-                now = new Date();
-
-            supressRatioCheck = !!supressRatioCheck;
-
-            //do not fire this handler for multiple times at startup
-            if (now - self.lastResize < 10){
-                return;
-            }
-
-            self.lastResize = now;
-
-            _.each(docs, function(doc){
-                documentsNameWidth += $(doc).width();
-            });
-
-            //do not enlarge tabs
-            if (ratio >= 1 && !supressRatioCheck){
-                return;
-            }
-
-            //do not enlarge tabs
-            if (ratio > 1){
-                ratio = 1;
-            }
-			
-			//prevent tabs from collapsing
-            if (ratio < 0){
-                ratio = 1;
-            }
-
-            _.each(docs, function(doc){
-                var $doc = $(doc),
-                    width = $doc.width();
-
-                $doc.width(width * (1 - (1 - ratio) / documentsNameWidth * docHolderWidth));
-            });
+            //no need for that code, we use flex now;
+            return true;
         }
 
         this.internalSizeHandler = function(){
             var docs = $('.document-holder .document-name');
-
-            //do not resize tabs if mouse is nearby
-            if (indicator.check('ext-documents')){
-                indicator.setHandler('ext-documents', self.tabResizeWorker);
-                return false;
-            }
 
             _.each(docs, function(doc){
                 var $doc = $(doc);
@@ -76,36 +34,12 @@ define(function(require, exports, module){
         }
 
         this.sizeHandler = function(suppress){
-            if (self.internalSizeHandler()){
-                setTimeout(function(){
-                    self.tabResizeWorker(suppress);
-                }, 1);
-            }
+            return true;
         }
 
         this.sizeHandlerSync = function(){
-            if (self.internalSizeHandler()){
-                self.tabResizeWorker();
-            }
+            return true;
         }
-
-        $MainViewManager.on('workingSetAdd', this.sizeHandler);
-        $MainViewManager.on('workingSetRemove', this.sizeHandler);
-        $MainViewManager.on('workingSetAddList', this.sizeHandler);
-        $MainViewManager.on('workingSetRemoveList', this.sizeHandler);
-        $DocumentManager.on('pathDeleted', this.sizeHandler);
-        $DocumentManager.on('workingSetSort', this.sizeHandler);
-        $MainViewManager.on('paneCreate', this.sizeHandler);
-        $WorkspaceManager.on('workspaceUpdateLayout', this.sizeHandler);
-        /*$WorkspaceManager.on('workspaceUpdateLayout', function(){
-            console.log('+');
-        });*/
-
-        $(window).on('resize', self.sizeHandler);
-
-        AppInit.appReady(function(){
-            self.sizeHandler();
-        });
     }
 
     module.exports = instance;
